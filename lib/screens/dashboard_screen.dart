7fe340530ab960230eb93e9124d27b6c44a65eaa
mobileapp-infinity <75470722+mobileapp-infinity@users.noreply.github.com>
2022-05-31 16:41:38 +0530

    dashboard starting

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -12,7 +12,65 @@ class _DashboardScreenState extends State<DashboardScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Infinty hr"),
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: Builder(
+          builder: (ctx) => IconButton(
+            icon: Icon(Icons.menu),
+            color: Color(0xffE21E23),
+            onPressed: () {
+              Scaffold.of(ctx).openDrawer();
+            },
+          ),
+        ),
+        title: const Text(
+          "INFINITY",
+          style: TextStyle(color: Colors.black),
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: SizedBox(
+              child: Image.asset(
+                "assets/images/logo.png",
+                scale: 2.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              child: Image.asset(
+                "assets/images/logo.png",
+                fit: BoxFit.cover,
+                scale: 1,
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+              ),
+              title: const Text('Page 1'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.train,
+              ),
+              title: const Text('Page 2'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
