3130ae01a38592b2914690dc0e0077f3912cb42b
Remish Varsani <scspl.remish@gmail.com>
2022-06-06 17:35:04 +0530

    base url changed

--- lib/api/api_urls.dart
+++ lib/api/api_urls.dart
@@ -1,4 +1,4 @@
 class ApiUrls {
   //TODO LIVE
-  static const String baseUrl = "http://iipl.iipl.info/ierphr.asmx/";
+  static const String baseUrl = "http://cu.ierp.co.in/ierphr.asmx/";//As per discussion with pragna madam base url changed
 }
